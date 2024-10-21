d369ec526fb27e7e6c9fe9db1c35c49927a7dbaf
Taylor Brockman <taylor.brockman@brainpowersoftware.com>
2024-10-21 17:09:25 -0400

    Prototype - Demo2 of d3.js of drawing Worldlines.

--- prototypes/d3js/d3demo2.js
+++ prototypes/d3js/d3demo2.js
@@ -18,13 +18,14 @@ const maxX = 10;
 const minT = -10;
 const maxT = 10;
 
+// Translation of coordinate systems = scaleLinear.domain and .range
 // Declare the x (horizontal position) scale.
-const x = d3.scaleLinear()
-    .domain([minX, maxX])
-    .range([marginLeft, width - marginRight])
+const xScale = d3.scaleLinear()
+    .domain([minX, maxX])  // Physics Coordinate system
+    .range([marginLeft, width - marginRight]) // Viewport Coordinate system.
 
 // Declare the y (vertical position) scale.
-const y = d3.scaleLinear()
+const tScale = d3.scaleLinear()
     .domain([minT, maxT])
     .range([height - marginBottom, marginTop]);
 
@@ -35,13 +36,13 @@ const svg = d3.create("svg")
 
 // Add the x-axis.
 svg.append("g")
-    .attr("transform", `translate(0,${((height)/2)-0})`)
-    .call(d3.axisBottom(x));
+    .attr("transform", `translate(0,${((height)/2)})`)
+    .call(d3.axisBottom(xScale));
 
 // Add the y-axis.
 svg.append("g")
     .attr("transform", `translate(${(width)/2},0)`)
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(tScale));
 
 
 // Draw a light line
@@ -49,60 +50,38 @@ svg.append("g")
 // Draw a Light like line ct
 svg.append("line")
     .style("stroke", "gray")
-    .attr("x1", x(minX))
-    .attr("x2", x(maxX))
-    .attr("y1", y(minT))
-    .attr("y2", y(maxT));
+    .attr("x1", xScale(minX))
+    .attr("x2", xScale(maxX))
+    .attr("y1", tScale(minT))
+    .attr("y2", tScale(maxT));
 
 // Draw a Light like line -ct
 svg.append("line")
     .style("stroke", "gray")
-    .attr("x1", x(maxX))
-    .attr("x2", x(minX))
-    .attr("y1", y(minT))
-    .attr("y2", y(maxT));
+    .attr("x1", xScale(maxX))
+    .attr("x2", xScale(minX))
+    .attr("y1", tScale(minT))
+    .attr("y2", tScale(maxT));
 
 // Your World Line (Vertical)
 svg.append("line")
     .style("stroke", "red")
     .style("stroke-width", "3")
-    .attr("x1", x(0))
-    .attr("x2", x(0))
-    .attr("y1", y(minT))
-    .attr("y2", y(maxT));
+    .attr("x1", xScale(0))
+    .attr("x2", xScale(0))
+    .attr("y1", tScale(minT))
+    .attr("y2", tScale(maxT));
 
 // Other Reference Frame World Line (Vertical)
 svg.append("line")
     .style("stroke", "blue")
     .style("stroke-width", "3")
-    .attr("x1", x(-5))
-    .attr("x2", x(5))
-    .attr("y1", y(minT))
-    .attr("y2", y(maxT));
+    .attr("x1", xScale(-5))
+    .attr("x2", xScale(5))
+    .attr("y1", tScale(minT))
+    .attr("y2", tScale(maxT));
 
-/*
-// 2024-09-27 Brockman - Customization to add a few demonstraton points
-
-var points = [
-    { date: new Date("2023-07-15"), amount : 24 },
-    { date: new Date("2023-10-15"), amount : 49 },
-    { date: new Date("2023-03-15"), amount : 15 },
-    { date: new Date("2023-01-15"), amount : 88 },
-];
-
-svg.append("g")
-    .selectAll("dot")
-    .data(points)
-    .join("circle")
-    .attr("cx", function (d) { return x(d.date); })
-    .attr("cy", function (d) { return y(d.amount); })
-    .attr("r", 6.0 )
-    .style("fill", "#6943a2");
-
-points.forEach(function(d) {
-    console.log("Point: ", d);
-});
-*/
+// SVG: <line x1="255" x2="745" y1="990" y2="10" style="stroke: blue; stroke-width: 3;"></line>
 
 // Append the SVG element.
 container.append(svg.node());
